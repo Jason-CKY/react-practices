71a8835095c3c383da7a81bccf0ba5e089af8a9d
Jason Cheng <cheng.ky@csit.gov.sg>
2022-06-22 17:46:33 +0800

    Added navbar design

--- static-page/src/components/Navbar.js
+++ static-page/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import ReactLogo from '../images/react-logo-small.png'
+
 export function Navbar() {
     return (
-        <h1>This is the nav bar</h1>
+        <nav className="nav">
+            <div className="nav-topleft">
+                <img src={ReactLogo} className="nav-logo" alt="react-logo"/>
+                <h2 className="nav-title" >React Facts</h2>
+            </div>
+            <h3 className="nav-details">React Course - Project 1</h3>
+        </nav>
     )
 }
